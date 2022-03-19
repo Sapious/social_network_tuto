72ee2cc9ba9f6be23d5a66ed90492e9c4b72bb82
raed bahri <raedbahri90@gmail.com>
2022-03-19 11:20:36 +0100

    no js

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,10 @@
-import { Fragment, useEffect } from "react";
-import Helmet from "react-helmet";
+import { Fragment } from "react";
 import "./assets/css/color.css";
 import "./assets/css/responsive.css";
 import "./assets/css/style.css";
 function App() {
 	return (
 		<Fragment>
-			<Helmet>
-				<script src="./assets/js/custom.js" type="text/javascript" async />
-			</Helmet>
 			<div>hello world</div>
 		</Fragment>
 	);
